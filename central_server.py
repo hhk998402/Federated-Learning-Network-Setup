d6f5ec3f0a3b36cf608266bd397fff5da7df8a80
hhk998402 <hemant.hari.kumar@gmail.com>
2020-08-30 09:20:57 +0000

    Add server and client code

---
+++ central_server.py
@@ -0,0 +1,204 @@
+import numpy as np
+import random
+import cv2
+import os
+from imutils import paths
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import backend as K
+
+import requests
+import pickle
+import jsonpickle
+import json
+
+
+
+def load(paths, verbose=-1):
+    '''expects images for each class in seperate dir, 
+    e.g all digits in 0 class in the directory named 0 '''
+    data = list()
+    labels = list()
+    # loop over the input images
+    for (i, imgpath) in enumerate(paths):
+        # load the image and extract the class labels
+        im_gray = cv2.imread(imgpath, cv2.IMREAD_GRAYSCALE)
+        image = np.array(im_gray).flatten()
+        label = imgpath.split(os.path.sep)[-2]
+        # scale the image to [0, 1] and add to list
+        data.append(image/255)
+        labels.append(label)
+        # show an update every `verbose` images
+        if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+            print("[INFO] processed {}/{}".format(i + 1, len(paths)))
+    # return a tuple of the data and labels
+    return data, labels
+
+
+def create_clients(image_list, label_list, num_clients=10, initial='clients'):
+    ''' return: a dictionary with keys clients' names and value as 
+                data shards - tuple of images and label lists.
+        args: 
+            image_list: a list of numpy arrays of training images
+            label_list:a list of binarized labels for each image
+            num_client: number of fedrated members (clients)
+            initials: the clients'name prefix, e.g, clients_1 
+            
+    '''
+
+    #create a list of client names
+    client_names = ['{}_{}'.format(initial, i+1) for i in range(num_clients)]
+
+    #randomize the data
+    data = list(zip(image_list, label_list))
+    random.shuffle(data)
+
+    #shard data and place at each client
+    size = len(data)//num_clients
+    shards = [data[i:i + size] for i in range(0, size*num_clients, size)]
+
+    #number of clients must equal number of shards
+    assert(len(shards) == len(client_names))
+
+    return {client_names[i] : shards[i] for i in range(len(client_names))}
+
+
+
+def batch_data(data_shard, bs=32):
+    '''Takes in a clients data shard and create a tfds object off it
+    args:
+        shard: a data, label constituting a client's data shard
+        bs:batch size
+    return:
+        tfds object'''
+    #seperate shard into data and labels lists
+    data, label = zip(*data_shard)
+    dataset = tf.data.Dataset.from_tensor_slices((list(data), list(label)))
+    return dataset.shuffle(len(label)).batch(bs)
+
+
+class SimpleMLP:
+    @staticmethod
+    def build(shape, classes):
+        model = Sequential()
+        model.add(Dense(200, input_shape=(shape,)))
+        model.add(Activation("relu"))
+        model.add(Dense(200))
+        model.add(Activation("relu"))
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+        return model
+    
+
+def weight_scalling_factor(clients_trn_data, client_name):
+    client_names = list(clients_trn_data.keys())
+    #get the bs
+    bs = list(clients_trn_data[client_name])[0][0].shape[0]
+    #first calculate the total training data points across clinets
+    global_count = sum([tf.data.experimental.cardinality(clients_trn_data[client_name]).numpy() for client_name in client_names])*bs
+    # get the total number of data points held by a client
+    local_count = tf.data.experimental.cardinality(clients_trn_data[client_name]).numpy()*bs
+    return local_count/global_count
+
+
+def scale_model_weights(weight, scalar):
+    '''function for scaling a models weights'''
+    weight_final = []
+    steps = len(weight)
+    for i in range(steps):
+        weight_final.append(scalar * weight[i])
+    return weight_final
+
+
+
+def sum_scaled_weights(scaled_weight_list):
+    '''Return the sum of the listed scaled weights. The is equivalent to scaled avg of the weights'''
+    avg_grad = list()
+    #get the average grad accross all client gradients
+    for grad_list_tuple in zip(*scaled_weight_list):
+        layer_mean = tf.math.reduce_sum(grad_list_tuple, axis=0)
+        avg_grad.append(layer_mean)
+        
+    return avg_grad
+
+
+def test_model(X_test, Y_test,  model, comm_round):
+    cce = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    #logits = model.predict(X_test, batch_size=100)
+    logits = model.predict(X_test)
+    loss = cce(Y_test, logits)
+    acc = accuracy_score(tf.argmax(logits, axis=1), tf.argmax(Y_test, axis=1))
+    print('comm_round: {} | global_acc: {:.3%} | global_loss: {}'.format(comm_round, acc, loss))
+    return acc, loss
+
+
+comms_round = 100
+
+#create optimizer
+lr = 0.01 
+loss='categorical_crossentropy'
+metrics = ['accuracy']
+optimizer = SGD(lr=lr, 
+                decay=lr / comms_round, 
+                momentum=0.9
+               ) 
+
+#initialize global model
+smlp_global = SimpleMLP()
+global_model = smlp_global.build(784, 10)
+
+with open('X_test.pkl', 'rb') as f:
+  X_test = pickle.load(f)
+
+with open('y_test.pkl', 'rb') as f:
+  y_test = pickle.load(f)
+
+#process and batch the test set  
+test_batched = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(len(y_test))
+
+ngrok_url='http://localhost:5000'
+#commence global training loop
+for comm_round in range(comms_round):
+            
+    # get the global model's weights - will serve as the initial weights for all local models
+    global_weights = global_model.get_weights()
+    
+    #initial list to collect local model weights after scalling
+    scaled_local_weight_list = list()
+
+    #randomize client data - using keys
+    client_names = ['client_1','client_2','client_3','client_4','client_5','client_6','client_7','client_8','client_9','client_10']
+    random.shuffle(client_names)
+    
+    #loop through each client and create new local model
+    for client in client_names:
+      print("sending for client "+client)
+      weights = jsonpickle.encode(global_weights)
+      resp = requests.post(ngrok_url+'/sendWeights', json = {'data' : weights,'client':client})
+      #print("response json encode:"+resp.text)
+      scaled_weights=jsonpickle.decode(resp.text)
+      scaled_local_weight_list.append(scaled_weights)
+        
+    #to get the average over all the local model, we simply take the sum of the scaled weights
+    average_weights = sum_scaled_weights(scaled_local_weight_list)
+    
+    #update global model 
+    global_model.set_weights(average_weights)
+
+    #test global model and print out metrics after each communications round
+    for(X_test, Y_test) in test_batched:
+        global_acc, global_loss = test_model(X_test, Y_test, global_model, comm_round)
+
+
